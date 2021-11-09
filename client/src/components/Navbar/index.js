774c909d1b10024446001d56767929f823807667
Jessica White <jessica2714white@gmail.com>
2021-11-08 19:56:28 -0600

    Created a Nav index.js that will display different components of the navbar

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Nav, NavLink, Bars, NavMenu, NavLink, NavBtn, NavBtnLink} from './NavbarElements';
+
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavLink to="/">
+                    <h1>Logo</h1>
+                    {/* <img src="" alt=""></img> */}
+                </NavLink>
+                <Bars />
+                <NavMenu>
+                    <NavLink to="/about" activeStyle>
+                        About
+                    </NavLink>
+                    {/* Add more navlinks */}
+
+                </NavMenu>
+            <NavBtn>
+            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+            </NavBtn>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar;
