7170c5a0bb3f06a5db6b16938e757b7e6fcf34b9
Jessica White <jessica2714white@gmail.com>
2021-11-15 16:41:52 -0600

    Making code look pretty

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import AboutUs from './pages/AboutUs';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import Footer from './components/Footer';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -15,22 +12,19 @@ const client = new ApolloClient({
 });
 
 function App() {
+
   return (
     <ApolloProvider client={client}>
       {/* Wrap page elements in Router component to keep track of location state */}
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
-          <Navbar />
-          <Header />
           <div className="container">
             {/* Define routes to render different page components at different paths */}
             <Route exact path="/" component={Home} />
             {/* Define a route that will take in variable data */}
             <Route exact path="/profiles/" component={Profile} />
             <Route exact path="/aboutus/" component={AboutUs}/>
-
           </div>
-          <Footer />
         </div>
       </Router>
     </ApolloProvider>
