074a0e04af4f6c520eb7e9582646240c337694ec
Jessica White <jessica2714white@gmail.com>
2021-11-15 21:36:06 -0600

    Installed packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './pages/Home';
