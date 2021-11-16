a20cdbe94976084ec3aa2dbdb20c7babbfcf38e6
Jessica White <jessica2714white@gmail.com>
2021-11-15 20:44:40 -0600

    Create Sidebar (activated in responsive mode)

---
+++ client/src/components/Sidebar/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+  SidebarContainer,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SidebarRoute,
+  SideBtnWrap
+} from './SidebarElements';
+
+const Sidebar = ({ isOpen, toggle }) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      {/* <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon> */}
+      <SidebarWrapper>
+        <SidebarMenu>
+        <SidebarLink
+            to='profile'
+            onClick={toggle}
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+          >
+            Profile
+          </SidebarLink>
+          <SidebarLink
+            to='about'
+            onClick={toggle}
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+          >
+            About Us
+          </SidebarLink>
+          <SidebarLink
+            to='signup'
+            onClick={toggle}
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+          >
+            Sign Up
+          </SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to='/signin'>Sign In</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;
