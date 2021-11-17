64ffb02fa91fe26235d73c80f1c174a6f7a26a87
Jessica White <jessica2714white@gmail.com>
2021-11-16 22:24:48 -0600

    Remove unnecessary files not being called

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,13 @@
 import React, {useState} from 'react';
 import { useQuery } from '@apollo/client';
 import Navbar from '../components/Navbar';
-import Header from '../components/Header';
+// import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
 import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data';
-import LogIn from '../components/LogIn';
-import SignUp from '../components/SignIn';
+// import LogIn from '../components/LogIn';
+// import SignUp from '../components/SignIn';
 import Footer from '../components/Footer';
 
 
@@ -24,13 +24,13 @@ const Home = () => {
       <div className="flex-row justify-center">
         <Sidebar isOpen={isOpen} toggle={toggle} />
         <Navbar toggle={toggle} />
-        <Header />
+        {/* <Header /> */}
         <HeroSection />
         <InfoSection {...homeObjOne} />
         <InfoSection {...homeObjTwo} />
-        <LogIn />
+        {/* <LogIn />
         <SignUp />
-        <script src='/login.js'/> 
+        <script src='/login.js'/>  */}
         <InfoSection {...homeObjThree} />
       </div>
       <Footer />
