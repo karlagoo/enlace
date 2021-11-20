ba37e9ddd2fe2784de92fb762f9e8ad0eec7c13c
Ham Hock <mheer91@icloud.com>
2021-11-20 01:37:24 -0600

    refined chat, more styling updates

--- client/src/components/LogIn/index.js
+++ client/src/components/LogIn/index.js
@@ -1,64 +1,81 @@
 import React, { useState } from 'react'
-import { useMutation, useQuery } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import { USER_LOGIN } from '../../utils/mutations';
 import Auth from '../../utils/auth';
+import { Container, Form } from 'react-bootstrap';
+
 
 const LogIn = () => {
 
-const [userLogIn, { error }] = useMutation(USER_LOGIN);
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
+    const [userLogIn, { error }] = useMutation(USER_LOGIN);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [validated, setValidated] = useState(false);
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        console.log(name, value)
 
-const handleInputChange = (e) => {
-    const { name, value } = e.target;
+        return name === 'email' ? setEmail(value) : setPassword(value);
+    };
 
-    return name === 'email' ? setEmail(value) : setPassword(value);
-  };
-
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
-
-    try{
-        const response  = await userLogIn({
-            variables: { email: email, password: password },
-        });
-
-        const token = response.data.login.token;
-        Auth.login(token);
-     
+    const handleValidation = (e) => {
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.stopPropagation();
+        }
+        setValidated(true);
     }
-    catch(err){
-        console.log(err);
-    }
-   
-  };
+
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await userLogIn({
+                variables: { email: email, password: password },
+            });
+
+            const token = response.data.login.token;
+            Auth.login(token);
+
+        }
+        catch (err) {
+            console.log(err);
+        }
+
+    };
 
     return (
         <div>
-        <div className='card'>
-            <div className='card-body'>
-                <h5 className='card-title'>Log In</h5>
-            <form id="logInForm">
-                <div className="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" name='email' id="emailInput" aria-describedby="emailHelp" onChange={handleInputChange} placeholder="Enter email"/>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" name='password' id="passwordInput" onChange={handleInputChange} placeholder="Password"/>
-                </div>
-                <div className="form-check">
-
-                </div>
-                <button type="submit" className="btn btn-primary" onClick={handleFormSubmit}>Submit</button>
-            </form>
-            </div>
-        </div>
-        
-      
-        
-        
-        
+            <Container>
+                <hr />
+                <h1 className="text-white">Log In</h1>
+                <Form noValidate validated={validated} onChange={handleValidation}>
+                    <Form.Group className="mb-3" controlId="email">
+                        <Form.Label className="text-white">Email</Form.Label>
+                        <Form.Control
+                            required
+                            type="email"
+                            placeholder="Enter Email"
+                            name="email"
+                            onChange={handleInputChange}
+                        />
+                        <Form.Control.Feedback type="invalid">Please enter a valid email address!</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="password">
+                        <Form.Label className="text-white">Password</Form.Label>
+                        <Form.Control
+                            required
+                            type="password"
+                            placeholder="Enter Password"
+                            name="password"
+                            onChange={handleInputChange}
+                        />
+                        <Form.Control.Feedback type="invalid">Please enter a password!</Form.Control.Feedback>
+                    </Form.Group>
+                    <button type="submit" className="btn btn-primary" onClick={handleFormSubmit} style={{ backgroundColor: '#449342', borderColor: "#449342", borderRadius: "15px" }}>Log In!</button>
+                </Form>
+            </Container>
+            <hr />
         </div>
     )
 }
